84cb51f5574b96b259980122a19c0cf953c59c61
LevviathaN <BillyJoe1234>
2019-02-22 16:46:09 +0200

    First training task created

---
+++ TrainingProject/HomePage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace TrainingProject
+{
+    public class HomePage
+    {
+        //Class attributes
+        private string pdp = "Mattress";
+
+        //Class methods
+        public void openPDP()
+        {
+            Console.WriteLine(pdp + " PDP is opened");
+        }
+    }
+}
---
+++ TrainingProject/ProductPage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace TrainingProject
+{
+    public class ProductPage
+    {
+        //Class attributes
+        private string btnLocator = "/some/btn/locator";
+
+        //Class methods
+        public void clickOnBtn()
+        {
+            Console.WriteLine("Clicking on button, located by: " + btnLocator);
+        }
+    }
+}
